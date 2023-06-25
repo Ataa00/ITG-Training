a814962f4e088fef16a46d15a027fc885edd308c
Ataa00 <ataa.shaqour.as@gmail.com>
2023-06-25 17:32:27 +0300

    Building RESTful APIs Using Express: - Handling POST requests - Handling PUT request

--- restExample/index.js
+++ restExample/index.js
@@ -1,5 +1,8 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 let courses = [
     {id : 1, name : "Course1"},
@@ -13,6 +16,39 @@ app.get("/", (request, response) => {
 
 app.get("/api/courses", (request, response) => {
     response.send(["C++", "JS", "Java", "Pythone"]);
+});
+
+app.post("/api/courses", (request, response) => {
+    //Add validation part 
+    // if(!req.body.name || req.body.name.length < 3){
+    //     res.status(400).send("Please Enter name with min. 3 characters");
+    // }
+
+    // const schem = {
+    //     name : Joi.string().min(3).required()
+    // };
+
+    // const result = Joi.validate(req.body, schem);
+    // console.log(result);
+
+    // if(result.error){
+    //     res.status(400).send(result.error);
+    //     return;
+    // }
+
+    const {error} = validateCourse(request.body);
+
+    if(error){
+        response.status(400).send(error);
+        return;
+    }
+
+    const course = {
+        id : courses.length+1,
+        name : request.body.name
+    };
+    courses.push(course);
+    response.send(course);
 });
 
 // //To send a paramtere we use ":".
@@ -29,9 +65,39 @@ app.get("/api/courses/:id", (request, response) => {
     response.send(course);
 });
 
+app.put("/api/courses/:id", (request, response) =>{
+    //Lockup the course
+    //If it is not existing, return 404.
+    const course = courses.find(c => c.id === parseInt(request.params.id));
+    if(!course){
+        response.status(404).send("The Course ID you Provided Is Not Exist.");
+    }
+
+    //if invalid, return 400 - bad request
+    const {error} = validateCourse(request.body);
+
+    if(error){
+        response.status(400).send(error.details[0].message);
+        return;
+    }
+
+    //Update course 
+    course.name = request.body.name;
+    //return update course
+    response.send(course);
+});
 
 //To set the Env variable we use in terminal => set => Windows or export for Linux " set PORT=5000"
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listning to port ${port}...\nOpen home page: http://localhost:${port}/`);
 });
+
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name : Joi.string().min(3).required()
+    });
+
+    return schema.validate(course);
+}
