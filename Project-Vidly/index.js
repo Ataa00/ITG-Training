bd07c3c503cf48deab3f118981c63468dab76d04
Ataa00 <ataa.shaqour.as@gmail.com>
2023-06-26 12:23:04 +0300

    Added CRUD functions for geners

--- Project-Vidly/index.js
+++ Project-Vidly/index.js
@@ -4,7 +4,30 @@ const express = require("express");
 
 const app = express();
 
-const genres = ["All", "Action", "Comedy", "Horror", "Adveture"];
+app.use(express.json());
+
+const genres = [
+    {
+        "id" : 1,
+        "type":"All"
+    },
+    {
+        "id" : 1,
+        "type":"Action"
+    },
+    {
+        "id" : 1,
+        "type":"Comedy"
+    },
+    {
+        "id" : 1,
+        "type":"Horror"
+    }, 
+    {
+        "id" : 1,
+        "type":"Adveture"
+    }
+];
 
 var movies = [
     {
@@ -33,27 +56,77 @@ app.get("/", (req, res) => {
     res.send(movies);
 });
 
+
 app.get("/api/genres", (req, res) => {
     res.send(movies);
 });
 
 app.get("/api/genres/:type", (req, res) => {
-    const type = geners.filter(g => g === req.params.type);
 
+    const genre = genres.find(g => g.type === req.params.type);
+    if(!genre) return res.status(404).send("This genre doesn't exist.")
 
-    
-    const moviesWithSameType = movies.filter(m =>  m.type );
-
-
+    res.send(movies.filter(m =>  m.type === genre.type));
 });
 
-function validateTypeOfMovies(type){
+app.post("/api/genres/addGenre", (req, res) => {
+
+    const genre = genres.find(g => g.type === req.body.type);
+    console.log(genre);
+    if(genre) return res.status(404).send("This genre is already exist.");
+    
+    const { error } = validateTypeOfMovies(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    genres.push({
+        "id" : genres.length+1,
+        "type" : req.body.type
+    });
+    res.send(genres);
+});
+
+app.put("/api/genres/:type", (req, res) => {
+
+    const genre = genres.find(g => g.type === req.params.type);
+
+    if(!genre) return res.status(404).send("This genre doesn't exist.");
+    
+    const { error } = validateTypeOfMovies(req.body);
+    
+    if(error) return res.status(400).send(error.details[0].message);
+
+    genre.type = req.body.type;
+
+    res.send(genre);
+});
+
+app.delete("/api/genres/:type", (req, res) => {
+
+    const genre = genres.find(g => g.type === req.params.type);
+
+    if(!genre) return res.status(404).send("This genre doesn't exist.");
+    
+    const { error } = validateTypeOfMovies(req.params);
+    
+    if(error) return res.status(400).send(error.details[0].message);
+
+    genres.splice(genre, 1);
+
+    res.send(genres);
+});
+
+
+function validateTypeOfMovies(genre){
     const schema = Joi.object({
         type : Joi.string().required().max(20)
     });
 
-    return schema.validate(type);
+    return schema.validate(genre);
 };
 
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listning to port ${port}...\nOpen home page: http://localhost:${port}/`);
+});
