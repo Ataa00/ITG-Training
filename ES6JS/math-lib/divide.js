41459bbe9e5cd1e3fcb7c05e23080066a5712220
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-05 16:47:34 +0300

    Divide by 0 handled

--- ES6JS/math-lib/divide.js
+++ ES6JS/math-lib/divide.js
@@ -1,5 +1,7 @@
 
 export default(firstNumber, secondNumber) => {
+    if(Number(secondNumber) === 0) return NaN;
+
     let result = Number(firstNumber) / Number(secondNumber);
     return result.toFixed(2);
 }
