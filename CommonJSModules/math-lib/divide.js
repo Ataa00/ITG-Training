a0804341a9ff9a488f052c66c088c5d4b6c1fbef
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-05 15:43:17 +0300

    Divide by 0 Handled

--- CommonJSModules/math-lib/divide.js
+++ CommonJSModules/math-lib/divide.js
@@ -1,6 +1,10 @@
 
 function divide(firstNumber, secondNumber){
+    
+    if(Number(secondNumber) === 0) return NaN;
+    
     let result = Number(firstNumber) / Number(secondNumber);
+    
     return result.toFixed(2);
 }
 
