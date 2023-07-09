0b88b3c13f43bf713c0fca208125b00f36670b19
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-09 14:49:38 +0300

    Converted exercise from callback aproach to async and await aproach

--- PracticeWorkTask-AsyncJS/exercise.js
+++ PracticeWorkTask-AsyncJS/exercise.js
@@ -1,35 +1,47 @@
 
-getCustomer(1, (customer) => {
-  console.log('Customer: ', customer);
-  if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log('Top movies: ', movies);
-      sendEmail(customer.email, movies, () => {
-        console.log('Email sent...')
-      });
-    });
+async function notifyCustomer(){
+  try{
+      const customer = await getCustomer(1);
+      console.log(customer);
+    if(customer.isGold){
+      const movies = await getTopMovies();
+      console.log("Top movies: ", movies);
+      await sendEmail(customer.email, movies);
+      console.log("Email sent...");
+    }
   }
-});
-
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({ 
-      id: 1, 
-      name: 'Mosh Hamedani', 
-      isGold: true, 
-      email: 'email' 
-    });
-  }, 4000);  
+  catch(error){
+    console.log("Error: ", error.message);
+  }
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(['movie1', 'movie2']);
-  }, 4000);
+notifyCustomer();
+
+function getCustomer(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ 
+        id: 1, 
+        name: 'Ataa Shaqour', 
+        isGold: true, 
+        email: 'ataa.shaqour.as@gmail.com' 
+      });
+    }, 4000);  
+  });
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function getTopMovies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['movie1', 'movie2']);
+    }, 4000);
+  });
+}
+
+function sendEmail(email, movies) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
 }
