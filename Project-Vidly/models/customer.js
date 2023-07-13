f3340fdb3b7067d7799f99fa3dbb9ce82e548dbd
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-13 16:07:22 +0300

    Add Custumer model

---
+++ Project-Vidly/models/customer.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const Customer = mongoose.model(
+    "Customer", 
+    new mongoose.Schema({
+        name: {
+            type: String,
+            required: true,
+            minlength: 2,
+            maxlength: 30
+        },
+        phoneNumber: {
+            type: String,
+            match: /[0-9]/,
+            minlength: 14,
+            maxlength: 14
+        },
+        isGolden: Boolean
+    })
+);
+
+export default Customer;
