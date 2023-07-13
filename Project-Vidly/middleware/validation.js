03a17dbdc46c427fe28b5bddad72dd3f34163143
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-13 11:41:12 +0300

    Added validation for GenreID and GenreType

--- Project-Vidly/middleware/validation.js
+++ Project-Vidly/middleware/validation.js
@@ -2,8 +2,16 @@
 import Joi from "joi"
 
 export function validateTypeOfMovies(genre){
-    const schema = Joi.object({
-        type : Joi.string().required().max(20)
+    const schema = Joi.object({ 
+        type: Joi.string().required().max(20)
+    });
+
+    return schema.validate(genre);
+};
+
+export function validateGenreID(genre){
+    const schema = Joi.object({ 
+        genreID: Joi.string().required().max(30)
     });
 
     return schema.validate(genre);
