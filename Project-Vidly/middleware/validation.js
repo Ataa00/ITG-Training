10d2a4f1f4c3b221335c16b4876d8393ea372a00
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-13 16:08:49 +0300

    Add Customer validation for ID and Its properties

--- Project-Vidly/middleware/validation.js
+++ Project-Vidly/middleware/validation.js
@@ -16,3 +16,21 @@ export function validateGenreID(genre){
 
     return schema.validate(genre);
 };
+
+export function validateCustomer(customer){
+    const schema = Joi.object({
+        name: Joi.string().min(2).max(30).required(),
+        phoneNumber: Joi.string().min(14).max(14).regex(/[0-9]/),
+        isGolden: Joi.boolean()
+    });
+
+    return schema.validate(customer);
+}
+
+export function validateCustomerID(customerID){
+    const schema = Joi.object({ 
+        customerID: Joi.string().required().max(24).min(24)
+    });
+
+    return schema.validate(customerID);
+};
