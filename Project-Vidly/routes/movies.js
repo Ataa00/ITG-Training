5171f0be11b88641604aa7bf115914c898eab163
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-17 11:59:42 +0300

    Corrected movies endpoints path according to the API path standards

--- Project-Vidly/routes/movies.js
+++ Project-Vidly/routes/movies.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 router.get("/", async (req, res) => await getMovies(req, res));
 
-router.get("/getMovie", async (req, res) => await getMovie(req, res));
+router.get("/:movieID", async (req, res) => await getMovie(req, res));
 
-router.post("/addMovie", async (req, res) => await createMovie(req, res));
+router.post("/", async (req, res) => await createMovie(req, res));
 
-router.put("/updateMovie", async (req, res) => await updateMovie(req, res));
+router.put("/:movieID", async (req, res) => await updateMovie(req, res));
 
-router.delete("/deleteMovie", async (req, res) => await deleteMovie(req, res));
+router.delete("/:movieID", async (req, res) => await deleteMovie(req, res));
 
 export default router;
