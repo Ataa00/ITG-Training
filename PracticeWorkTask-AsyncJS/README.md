a8d184d801ab01cc479e50bdaef10d2266385d7f
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-18 12:18:30 +0300

    - Made a movies list and change between them randomly - Integrate an actual email service

--- PracticeWorkTask-AsyncJS/README.md
+++ PracticeWorkTask-AsyncJS/README.md
@@ -2,9 +2,22 @@
 
 # Branch Description:
 
-In this branch aims to solve the follwing task.
+This branch aims to solve the follwing task.
 
 # Task: AsyncJS
 ---
-The exercise file has code written in callback aproach.  
-I should convert this aproach to async and await aproach.
+- The "exercise.js" file has code written in a callback approach.  
+I should convert this approach to an async and await approach.
+- (Extra) Make a movie list instead of the static array and choose randomly.
+- (Extra) Integrate an actual email service.
+  - I used the nodemailer package for email services ```npm i nodemailer```
+  - First, I created a nodemailer transporter and made some configurations for the sender which are:
+    - Set the host provider => (smtp.office365.com, smtp.ethereal.email)
+    - Set the port number for the SMTP protocol.
+    - Set the user email and password
+  - Then, I made a function to create a message.
+  - Finally, Sent that message to the wanted user.
+  - I sent an email from an actual email.  
+    ![](emailSent.png)
+  - I sent an email from a virtual email
+    ![](SentFromVirtualEmail.png)
--- PracticeWorkTask-AsyncJS/exercise.js
+++ PracticeWorkTask-AsyncJS/exercise.js
@@ -1,3 +1,22 @@
+import nodeMailer from "nodemailer";
+
+const admin = {
+  name: "Ataa Shaqour",
+  email: "myrtis.maggio12@ethereal.email",
+  password: "tnbK8GGGfRqkjP2gMb"
+}
+
+const user = {
+  name: "Omar Dere",
+  email: "ataa.shaqour.as@gmail.com",
+  isGold: true,
+}
+
+const movies = [
+  'movie1',
+  'movie2',
+  'movie3'
+];
 
 async function notifyCustomer(){
   try{
@@ -6,7 +25,7 @@ async function notifyCustomer(){
     if(customer.isGold){
       const movies = await getTopMovies();
       console.log("Top movies: ", movies);
-      await sendEmail(customer.email, movies);
+      await sendEmail(customer, movies);
       console.log("Email sent...");
     }
   }
@@ -15,17 +34,12 @@ async function notifyCustomer(){
   }
 }
 
-notifyCustomer();
+notifyCustomer().catch(err => console.log(err.message));
 
 function getCustomer(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve({ 
-        id: 1, 
-        name: 'Ataa Shaqour', 
-        isGold: true, 
-        email: 'ataa.shaqour.as@gmail.com' 
-      });
+      resolve(user);
     }, 4000);  
   });
 }
@@ -33,15 +47,41 @@ function getCustomer(id) {
 function getTopMovies() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(['movie1', 'movie2']);
+      resolve(getRandomMovie(movies));
     }, 4000);
   });
 }
 
-function sendEmail(email, movies) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, 4000);
+async function sendEmail(user, movies) {
+  return new Promise(async (resolve, reject) => {
+    const transporter = createConnection();
+    const info = await transporter.sendMail(getMailOptions(user, "Top movies", `This is the top movie you choosed: ${movies}`));
+    resolve(console.log("Message sent: %s", info.messageId));
   });
+}
+
+function getRandomMovie(movies){
+  return movies[Math.floor(Math.random()*movies.length)]
+}
+
+function createConnection(){
+  return nodeMailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+    auth: {
+        user: admin.email,
+        pass: admin.password
+    },
+    stl: {rejectUnauthorized: false}
+  });
+}
+
+function getMailOptions(user, subject, text){
+  return {
+    from: `${admin.name} <${admin.email}>`, // sender address
+    to: user.email, // list of receivers
+    subject: subject, // Subject line
+    text: text, // plain text body
+  }
 }
