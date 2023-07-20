2e3040ba256035e4c16e43710794a210fda57e80
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-20 11:24:30 +0300

    Typo corrected

--- ES6JS/math-lib/substract.js
+++
@@ -1,5 +0,0 @@
-
-export default(firstNumber, secondNumber) => {
-    let result = Number(firstNumber) - Number(secondNumber);
-    return result.toFixed(2);
-}
---
+++ ES6JS/math-lib/subtract.js
@@ -0,0 +1,12 @@
+
+export default(firstNumber, secondNumber) => {
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    if(isNaN(firstNumber) || isNaN(secondNumber)){
+        return "Please enter a valid number";
+    }
+
+    let result = firstNumber - secondNumber;
+    return result.toFixed(2);
+}
