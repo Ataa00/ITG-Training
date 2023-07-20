b918941e02ca861424d0b17b4839eaef0383df16
Ataa00 <ataa.shaqour.as@gmail.com>
2023-07-20 11:13:38 +0300

    Send an error message

--- CommonJSModules/math-lib/divide.js
+++ CommonJSModules/math-lib/divide.js
@@ -1,9 +1,17 @@
 
 function divide(firstNumber, secondNumber){
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    if(isNaN(firstNumber) || isNaN(secondNumber)){
+        return "Please enter a valid number";
+    }
+
+    if(secondNumber === 0){
+        return "Illigel operation. Can't divide by zero.";
+    }
     
-    if(Number(secondNumber) === 0) return NaN;
-    
-    let result = Number(firstNumber) / Number(secondNumber);
+    let result = firstNumber / secondNumber;
     
     return result.toFixed(2);
 }
